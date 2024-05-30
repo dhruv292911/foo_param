9fde8a76695a117647caa7a1092232dfd7c952a4
dhruv292911 <dhruvshah2597@gmail.com>
2024-05-30 15:22:36 -0500

    Added Python Package Foo Paramterization

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='foo-parameterization',
+    version='0.1',
+    py_modules=['foo_parameterization'],
+    install_requires=[],  # Add any dependencies here
+    python_requires='>=3.6',
+    author='Your Name',
+    description='A package for Foo et al. parameterization',
+    url='https://github.com/yourusername/foo-parameterization',
+    license='MIT',
+)
